23c098f355957c1193ec7dacf11c2e36518d891d
Geeleed <surasak.kaewpho@gmail.com>
2025-08-09 08:58:38 +0700

    แก้ cors

--- index.js
+++ index.js
@@ -3,7 +3,33 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+
+const allowedOrigins = [
+  "http://localhost:3200",
+  "http://localhost:3201",
+  "https://otc.geeleed.com",
+  "https://api-otc.geeleed.com",
+];
+
+const corsConfig = {
+  credentials: true,
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      var msg =
+        "The CORS policy for this site does not " +
+        "allow access from the specified Origin.";
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+};
+
+app.use(cors(corsConfig));
+
+app.route("/").get(async (req, res) => {
+  res.redirect(process.env.FRONTEND_URL);
+});
 
 app.use("/auth", require("./routes/auth"));
 app.use("/utils", require("./routes/utils"));
